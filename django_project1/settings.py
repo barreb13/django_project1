81ea15698e4a09152ebe6f2ec54e8969d4d39a3e
Brent Barrese <brent.barrese@gmail.com>
2021-06-16 09:13:29 -0700

    update

--- django_project1/settings.py
+++ django_project1/settings.py
@@ -163,3 +163,5 @@ AWS_S3_FILE_OVERWRITE = False                       #users update same file name
 AWS_DEFAULT_ACL = None
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+django_heroku.settings(locals())
