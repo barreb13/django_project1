ed0b7c3ee7550e54facd628c7c3a339928f9fa47
Brent Barrese <brent.barrese@gmail.com>
2021-06-16 08:15:18 -0700

    update static root

--- django_project1/settings.py
+++ django_project1/settings.py
@@ -122,6 +122,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+#add for heroku
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+#for deploying to heroku
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')    #where uploaded files will be stored on the file system
