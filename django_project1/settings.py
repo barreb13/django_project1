2f201eaacb01ac150304292d6cdcc6664e74f4c6
Brent Barrese <brent.barrese@gmail.com>
2021-06-16 09:31:47 -0700

    udpate

--- django_project1/settings.py
+++ django_project1/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
-import django_heroku
+#import django_heroku
 
 from pathlib import Path
 
@@ -164,4 +164,4 @@ AWS_DEFAULT_ACL = None
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-django_heroku.settings(locals())
+#django_heroku.settings(locals())
